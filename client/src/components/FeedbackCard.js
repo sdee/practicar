9345b05558244d68bc0a1d78c13bfe66a5695d67
Jacob Madden <jacob.madden@gmail.com>
2017-02-01 01:37:38 -0500

    show correct answer with accents when user inputs correct answer while ignoring accents

--- client/src/components/FeedbackCard.js
+++ client/src/components/FeedbackCard.js
@@ -2,10 +2,11 @@ import React, { PropTypes } from 'react';
 
 function FeedbackCard({ isCorrect, correctAnswer, submittedAnswer }) {
 	const userAnswerClass = isCorrect ? 'correctAnswer' : 'incorrectAnswer';
+	const displayAnswer = isCorrect ? correctAnswer : submittedAnswer;
 	return (
 		<div>
 			<section className="back">
-				<span className={userAnswerClass}>{submittedAnswer}</span>
+				<span className={userAnswerClass}>{displayAnswer}</span>
 				<span className="answer">{isCorrect ? '' : ` -> ${correctAnswer}`}</span>
 			</section>
 		</div>
