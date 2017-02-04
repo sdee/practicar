d9b8d2a757a59c5f6b5cba6e4dd56479bdb6ee72
Jacob Madden <jacob.madden@gmail.com>
2017-02-03 22:34:52 -0500

    formatting fixes only

--- client/src/components/AnswerCard.js
+++ client/src/components/AnswerCard.js
@@ -2,10 +2,10 @@ import React, { PropTypes } from 'react';
 
 function createMarkup(answer, irregularity) {
 	var msg = [];
-	if (irregularity.length>0) {
+	if (irregularity.length > 0) {
 		const i = answer.indexOf(irregularity);
 		const j = i+irregularity.length;
-		const before = answer.slice(0,i).trim();
+		const before = answer.slice(0, i).trim();
 		const after = answer.slice(j,answer.length).trim();
 		msg = [before, '<span style="color:red">', irregularity, '</span>', after];
 	}
@@ -18,14 +18,13 @@ function createMarkup(answer, irregularity) {
 function AnswerCard({ answer, irregularity }) {
 	return (
 		<div>
-		<section className="card back">
-		<div>
-
-		<div dangerouslySetInnerHTML={createMarkup(answer, irregularity)} />
+			<section className="card back">
+				<div>
+					<div dangerouslySetInnerHTML={createMarkup(answer, irregularity)} />
+				</div>
+			</section>
 		</div>
-		</section>
-		</div>
-		);
+	);
 }
 
 AnswerCard.propTypes = {
