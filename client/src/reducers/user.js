b070b832adb639864dfbfc4e4e449a0430df35ac
Jacob Madden <jacob.madden@gmail.com>
2017-02-07 22:21:55 -0500

    increment question number tally every time next question is clicked

--- client/src/reducers/user.js
+++ client/src/reducers/user.js
@@ -1,7 +1,12 @@
-// import { SUBMIT_ANSWER } from '../actions';
+import { NEXT_QUESTION } from '../actions';
 
-const user = (state = '', action) => {
+const user = (state = { questionNo: 0 }, action) => {
 	switch (action.type) {
+  case NEXT_QUESTION: {
+    return {
+      questionNo: state.questionNo+1
+    };
+  }
 	default:
 		return state;
 	}
