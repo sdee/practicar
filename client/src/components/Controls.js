93093b9e376968d7c18564e4818f4c31b4b8cd7e
Sutee Dee <thaiyoshi@gmail.com>
2017-02-17 23:15:36 -0500

    refreshes look of buttons

--- client/src/components/Controls.js
+++ client/src/components/Controls.js
@@ -1,13 +1,13 @@
 import React, { PropTypes } from 'react';
-import { Button, ButtonGroup } from 'react-bootstrap';
+import { Button, ButtonGroup, Glyphicon } from 'react-bootstrap';
 
 function Controls({ onNextClick, onPrevClick, onShowClick, showAnswer }) {
 	const showButtonText = showAnswer ? 'See Question' : 'See Answer';
 	return (
 		<div>
 		<div>
-		<Button bsStyle="success" onClick={onPrevClick}>Previous Question</Button>
-			<Button bsStyle="success" onClick={onNextClick}>Next Question</Button>
+		<Button bsStyle="success" onClick={onPrevClick}><Glyphicon glyph="chevron-left"/>Back</Button>
+		<Button bsStyle="success" onClick={onNextClick}>Next<Glyphicon glyph="chevron-right"/></Button>
 		</div>
 		<br/>
 		<div>
