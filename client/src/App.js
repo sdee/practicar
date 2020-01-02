ec8de73736c18fba4c30668128ebd1b0ce6812a8
Sutee Dee <sutee.dee@invitae.com>
2020-01-01 17:06:23 -0800

    Removes client src and reinstalls create-react-app to latest

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,26 @@
 import React from 'react';
-import { Panel, Grid, Row, Col } from 'react-bootstrap';
+import logo from './logo.svg';
 import './App.css';
-import Readme from './components/Readme';
-import ShortcutInfo from './components/ShortcutInfo';
-import LinkControls from './containers/LinkControls';
-import CustomOptions from './containers/CustomOptions';
-import ControlledCard from './containers/ControlledCard';
-import UserAnswer from './containers/UserAnswer';
-import KeyboardControls from './containers/KeyboardControls';
 
 function App() {
-	return (
-		<KeyboardControls>
-			<div className="App">
-				<Panel header="Yo practico, tú practicas, nosotros practicamos">
-					<Grid>
-						<Row className="show-grid">
-							<Col md={7}>
-								<Row className="show-grid">
-									<ControlledCard />
-								</Row>
-								<Row className="card ctrl">
-									<br />
-									<LinkControls />
-								</Row>
-								<Row className="card ctrl">
-									<br />
-									<UserAnswer />
-									<ShortcutInfo />
-								</Row>
-							</Col>
-							<Col md={5}>
-								<CustomOptions />
-							</Col>
-						</Row>
-					</Grid>
-				</Panel>
-				<Readme />
-			</div>
-		</KeyboardControls>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
