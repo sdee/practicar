d21d870a390e4620432e1a07478af974cbeb3ed9
Sutee Dee <sutee.dee@invitae.com>
2020-01-01 17:37:43 -0800

    replaces src directory and upgrades node engine

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Panel, Grid, Row, Col } from 'react-bootstrap';
 import './App.css';
+import Readme from './components/Readme';
+import ShortcutInfo from './components/ShortcutInfo';
+import LinkControls from './containers/LinkControls';
+import CustomOptions from './containers/CustomOptions';
+import ControlledCard from './containers/ControlledCard';
+import UserAnswer from './containers/UserAnswer';
+import KeyboardControls from './containers/KeyboardControls';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<KeyboardControls>
+			<div className="App">
+				<Panel header="Yo practico, tú practicas, nosotros practicamos">
+					<Grid>
+						<Row className="show-grid">
+							<Col md={7}>
+								<Row className="show-grid">
+									<ControlledCard />
+								</Row>
+								<Row className="card ctrl">
+									<br />
+									<LinkControls />
+								</Row>
+								<Row className="card ctrl">
+									<br />
+									<UserAnswer />
+									<ShortcutInfo />
+								</Row>
+							</Col>
+							<Col md={5}>
+								<CustomOptions />
+							</Col>
+						</Row>
+					</Grid>
+				</Panel>
+				<Readme />
+			</div>
+		</KeyboardControls>
+	);
 }
 
 export default App;
