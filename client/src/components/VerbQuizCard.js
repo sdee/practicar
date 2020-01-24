66373ae48806d1de0f86fb5aef8456b6ab5d5ce0
Sutee Dee <sutee.dee@invitae.com>
2020-01-23 20:25:15 -0800

    Adds back hoverover for definition of verbs

--- client/src/components/VerbQuizCard.js
+++ client/src/components/VerbQuizCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import QuizCard from './QuizCard';
+import { OverlayTrigger, Tooltip, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const _ = require('lodash');
 
 const getBeforeAfter = (answer, irregularity) => {
@@ -18,7 +20,21 @@ const getBeforeAfter = (answer, irregularity) => {
 	return beforeAfter;
 }
 
-const formatQuestion = (pronoun, infinitive, tense, mood) => <>{pronoun}, <b>{infinitive}</b><br />({tense} {mood})</>
+const DefinitionHoverover = ({ definition }) => (
+	<OverlayTrigger
+		placement="right"
+		overlay={<Tooltip id={`tooltip`}>
+			{definition}
+		</Tooltip>}
+	>
+		<FontAwesomeIcon icon="question-circle" size="xs" />
+	</OverlayTrigger>
+);
+
+
+const formatQuestion = (pronoun, infinitive, tense, mood, definition) => {
+	return <>{pronoun}, <b>{infinitive}</b><br />({tense} {mood}) <DefinitionHoverover definition={definition} /></>
+}
 
 const formatAnswer = (answer, irregularity) => {
 	const beforeAfter = getBeforeAfter(answer, irregularity);
@@ -40,10 +56,10 @@ const VerbQuizCard = (props) => {
 		const tense = question.tense;
 		const mood = question.mood;
 		const irregularity = question.irregularity;
-		formattedQuestion = formatQuestion(pronoun, verb, tense, mood);
+		const definition = question.definition;
+		formattedQuestion = formatQuestion(pronoun, verb, tense, mood, definition);
 		answer = currentCard.answer;
-
-		formattedAnswer = formatAnswer(answer, irregularity)
+		formattedAnswer = formatAnswer(answer, irregularity);
 	}
 	return (
 		<QuizCard {...props} question={formattedQuestion} correctAnswer={formattedAnswer} />
