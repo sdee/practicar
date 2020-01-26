da077acb41eadc18f457901a4d3505dc602477d0
Jacob Madden <jacob.madden@gmail.com>
2020-01-26 02:34:47 -0500

    fix import ordering; remove quizType input from redux, now from path; fixed useEffect dependency from quiz to quiz.focus to stop repeat calls; style: space around equality, align blocks; added missing propTypes

--- client/src/containers/ControlledCard.js
+++ client/src/containers/ControlledCard.js
@@ -1,50 +1,51 @@
 import React, {useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import {usePath} from 'hookrouter';
 import NumbersQuizCard from '../components/NumbersQuizCard';
 import VerbQuizCard from '../components/VerbQuizCard';
 import { loadQuiz, flipCard } from '../actions';
-import {usePath} from 'hookrouter';
 
-const ControlledCard = ({dispatch, quiz, quizType, user}) => {
+const ControlledCard = ({dispatch, quiz, user}) => {
 	const cardRef = useRef();
 	const path = usePath();
 
+	const quizType = path.slice(1) || 'verbs';
+
 	useEffect(() => {
-		const quizType = path.slice(1) || 'verbs'; 
 		dispatch(loadQuiz(quizType));
 		if (quiz.focus === 'card' && cardRef) {
 			cardRef.current.focus();
 		}
-	  }, [path, dispatch, quiz]);
+	}, [quizType, dispatch, quiz.focus]);
 
 	let quizCard;
-	if (quizType==='numbers') {
+	if (quizType === 'numbers') {
 		quizCard = React.createElement(NumbersQuizCard, {...quiz, questionNum: user.questionNum})
-	}
-	else {
+	} else {
 		quizCard = React.createElement(VerbQuizCard, {...quiz, questionNum: user.questionNum})
 	}
-		return (
-			<div
-				id="card"
-				tabIndex="0"
-				ref={cardRef}
-				onClick={(e) => { dispatch(flipCard()); }}>
-				{quizCard}	
-			</div>
-		);
-	}
+	return (
+		<div
+			id="card"
+			tabIndex="0"
+			ref={cardRef}
+			onClick={(e) => { dispatch(flipCard()); }}>
+			{quizCard}
+		</div>
+	);
+}
 
 
 ControlledCard.propTypes = {
-	dispatch: PropTypes.func.isRequired
+	dispatch: PropTypes.func.isRequired,
+	quiz: PropTypes.object.isRequired,
+	user: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
 	quiz: state.quiz,
 	user: state.user,
-	quizType: state.quiz.type,
 });
 
 export default connect(mapStateToProps)(ControlledCard);
