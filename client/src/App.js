1828fa5e45ba7ce717350043626c3acf1eda5677
Sutee Dee <sutee.dee@invitae.com>
2020-01-26 07:31:49 -0800

    Split numbers and verbs into separate files

--- client/src/App.js
+++ client/src/App.js
@@ -1,86 +1,8 @@
 import React from 'react';
-import { Card, Container, Row, Col } from 'react-bootstrap';
 import './App.css';
-import Readme from './components/Readme';
-import ShortcutInfo from './components/ShortcutInfo';
-import LinkControls from './containers/LinkControls';
-import CustomOptions from './containers/CustomOptions';
-import ControlledCard from './containers/ControlledCard';
-import UserAnswer from './containers/UserAnswer';
-import KeyboardControls from './containers/KeyboardControls';
 import {useRoutes} from 'hookrouter';
-
-const Verbs = () => {
-	return (
-		<KeyboardControls>
-			<div className="App">
-				<Card style={{ width: '100%' }}>
-					<Card.Header>Yo practico, tú practicas, nosotros practicamos</Card.Header>
-					<Card.Body>
-						<Container>
-							<Row className="show-grid">
-								<Col md={7}>
-									<Row className="show-grid">
-										<ControlledCard />
-									</Row>
-									<Row className="ctrl">
-										<LinkControls />
-									</Row>
-									<Row className="ctrl">
-										<UserAnswer />
-									</Row>
-									<Row className="ctrl">
-										<ShortcutInfo />
-									</Row>
-								</Col>
-								<Col md={5}>
-									<CustomOptions />
-								</Col>
-							</Row>
-						</Container>
-					</Card.Body>
-				</Card>
-				<Readme />
-			</div>
-		</KeyboardControls>
-	);
-}
-
-const Numbers = () => {
-	return (
-		<KeyboardControls>
-			<div className="App">
-				<Card style={{ width: '100%' }}>
-					<Card.Header>Cuenta conmigo. Uno, dos, tres ...</Card.Header>
-					<Card.Body>
-					<Container>
-							<Row className="show-grid">
-								<Col md={7}>
-									<Row className="show-grid">
-										<ControlledCard />
-									</Row>
-									<Row className="ctrl">
-										<LinkControls />
-									</Row>
-									<Row className="ctrl">
-										<UserAnswer />
-									</Row>
-									<Row className="ctrl">
-										<ShortcutInfo />
-									</Row>
-								</Col>
-								{/* <Col md={5}>
-									<CustomOptions />
-								</Col> */}
-							</Row>
-						</Container>
-					</Card.Body>
-				</Card>
-				<Readme />
-			</div>
-		</KeyboardControls>
-	);
-}
+import Verbs from './Verbs'
+import Numbers from './Numbers'
 
 const routes = {
 	'/': () => <Verbs />,
