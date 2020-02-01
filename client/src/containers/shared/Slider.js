bdd271c741b117670aaea3900f04e28f381af872
Sutee Dee <sutee.dee@invitae.com>
2020-02-01 00:06:25 -0800

    Enhances formatting of slider scale

--- client/src/containers/shared/Slider.js
+++ client/src/containers/shared/Slider.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Nouislider from "nouislider-react";
 import "nouislider/distribute/nouislider.css";
+import wNumb from 'wnumb';
 import { Form, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { setFilter,  } from '../../actions';
 
 class Slider extends React.Component {
     onSet = (render, handle, value, un, percent) => {
-        this.props.setFilter('MIN_NUMBER', Number.parseInt(value[0]))
-        this.props.setFilter('MAX_NUMBER', Number.parseInt(value[1]))
+        console.log(value)
+        this.props.setFilter('MIN_NUMBER', Math.round(value[0]))
+        this.props.setFilter('MAX_NUMBER', Math.round(value[1]))
 
     };
     render() {
@@ -25,16 +27,24 @@ class Slider extends React.Component {
                     behaviour="tap"
                     range={{
                         min: [0],
-                        "16%": [50, 5],
-                        "33%": [100, 100],
-                        "66%": [1000, 100],
-                        "80%": [10000, 1000],
+                        "20%": [50, 5],
+                        "40%": [100, 100],
+                        "60%": [10000, 1000],
+                        "80%": [100000, 100000],
                         max: [1000000]
                     }}
                     onSet={this.onSet}
+                    pips={{ mode: "positions", values: [0, 20, 40, 60, 80, 100], density: 5, format: wNumb({
+                        decimals: 0,
+                        thousand: ','
+                    })}}
+                    clickablePips
+                    
+                 
                 />
                 <br />
                 <div class='sliderValue'>
+                <br/> 
                     Practice numbers between <b>{min.toLocaleString('en-US')}</b> and <b>{max.toLocaleString('en-US')}</b>
                 </div>
             </div>
